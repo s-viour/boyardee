615f181248346f34207f4dd818b1c0c0837254fe
Matt Wyatt <mwyatt1000@gmail.com>
2021-10-21 12:34:27 -0400

    fix formatting on index.js file to 4 spaces

--- index.js
+++ index.js
@@ -12,30 +12,30 @@ client.commands = new Collection();
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
-  const command = require(`./commands/${file}`);
-  client.commands.set(command.data.name, command);
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.data.name, command);
 }
 
 
 client.once('ready', () => {
-  console.log('ready!');
+    console.log('ready!');
 });
 
 client.on('interactionCreate', async (interaction) => {
-  if (!interaction.isCommand()) { return; }
+    if (!interaction.isCommand()) { return; }
+    
+    const command = client.commands.get(interaction.commandName);
+    if (!command) { return; }
 
-  const command = client.commands.get(interaction.commandName);
-  if (!command) { return; }
-
-  try {
-    await command.execute(interaction);
-  } catch (error) {
-    console.log(error);
-    await interaction.reply({
-      content: 'error executing command',
-      ephemeral: true
-    });
-  }
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.log(error);
+        await interaction.reply({
+            content: 'error executing command',
+            ephemeral: true
+        });
+    }
 });
 
 client.login(token)
